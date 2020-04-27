c99c9e53388288e11faca62822a789f1b9c1e3dc
Handerson Contreras <handerson.contreras@laureate.net>
2020-04-27 00:17:26 -0600

    adding firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAT1qHeh25aMM4NEpoJo7uHgQrSLcFCND4',
+    authDomain: 'gdg-tgu-angularfire-demo.firebaseapp.com',
+    databaseURL: 'https://gdg-tgu-angularfire-demo.firebaseio.com',
+    projectId: 'gdg-tgu-angularfire-demo',
+    storageBucket: 'gdg-tgu-angularfire-demo.appspot.com',
+    messagingSenderId: '239010130032',
+    appId: '1:239010130032:web:77ee560667f411ba57acb5',
+    measurementId: 'G-4DXDC61DFB'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAT1qHeh25aMM4NEpoJo7uHgQrSLcFCND4',
+    authDomain: 'gdg-tgu-angularfire-demo.firebaseapp.com',
+    databaseURL: 'https://gdg-tgu-angularfire-demo.firebaseio.com',
+    projectId: 'gdg-tgu-angularfire-demo',
+    storageBucket: 'gdg-tgu-angularfire-demo.appspot.com',
+    messagingSenderId: '239010130032',
+    appId: '1:239010130032:web:77ee560667f411ba57acb5',
+    measurementId: 'G-4DXDC61DFB'
+  }
 };
 
 /*
